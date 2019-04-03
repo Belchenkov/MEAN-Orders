c7d190ed3b6dea71d16a7a05cbab904edae0a6f7
Aleksey Belchenkov <u608110@gmail.com>
2019-04-03 06:32:23 +0300

    Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,35 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const keys = require('../config/keys');
 const User = require('../models/User');
 
-module.exports.login = function (req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
+module.exports.login = async function (req, res) {
+    const candidate = await User.findOne({ email: req.body.email });
+
+    if (candidate) {
+        // Check password
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+
+        if (passwordResult) {
+            // Generate Token
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, { expiresIn: 3600 });
+
+            res.status(200).json({ token: `Bearer ${token}` });
+        } else {
+            res.status(401).json({
+                message: 'Некорректный пароль'
+            })
         }
-    });
+    } else {
+        // Not User -> error
+        res.status(404).json({
+            message: 'Пользователь не найден'
+        });
+    }
 };
 
 module.exports.register = async function (req, res) {
