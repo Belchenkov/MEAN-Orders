66d5a089377277773fccd9230bfc7eb46f09955c
Aleksey Belchenkov <u608110@gmail.com>
2019-04-04 07:19:24 +0300

    Create Universal Handler Errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const keys = require('../config/keys');
 const User = require('../models/User');
+const errorHandler = require('../utils/errorHandler');
 
 module.exports.login = async function (req, res) {
     const candidate = await User.findOne({ email: req.body.email });
@@ -55,7 +56,7 @@ module.exports.register = async function (req, res) {
             await user.save();
             res.status(201).json(user);
         } catch (e) {
-
+            errorHandler(res, e);
         }
     }
 };
