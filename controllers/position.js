a5618e0fb3f3cc295046ba146f26cbd4ab710fda
Aleksey Belchenkov <u608110@gmail.com>
2019-04-06 10:16:44 +0300

    Get Position By Category

--- controllers/position.js
+++ controllers/position.js
@@ -1,7 +1,39 @@
-module.exports.getByCategoryId = (req, res) => {};
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
 
-module.exports.remove = (req, res) => {};
+module.exports.getByCategoryId = async (req, res) => {
+    try {
+        const positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        });
 
-module.exports.create = (req, res) => {};
+        res.status(200).json(positions);
+    } catch (err) {
+        errorHandler(res, err);
+    }
+};
 
-module.exports.update = (req, res) => {};
+module.exports.remove = (req, res) => {
+    try {
+
+    } catch (err) {
+        errorHandler(res, err);
+    }
+};
+
+module.exports.create = (req, res) => {
+    try {
+
+    } catch (err) {
+        errorHandler(res, err);
+    }
+};
+
+module.exports.update = (req, res) => {
+    try {
+
+    } catch (err) {
+        errorHandler(res, err);
+    }
+};
