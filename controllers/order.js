0e10413feb8c46f5f46665189d744440fc3019e2
Aleksey Belchenkov <u608110@gmail.com>
2019-04-07 11:05:02 +0300

    Create a new Order

--- controllers/order.js
+++ controllers/order.js
@@ -1,4 +1,25 @@
+const Order = require('../models/Order');
+const errorHandler = require('../utils/errorHandler');
+
 module.exports.getAll = (req, res) => {};
 
-module.exports.create = (req, res) => {};
+module.exports.create = async (req, res) => {
+    try {
+        const lastOrder = await Order
+            .findOne({user: req.user.id})
+            .sort({date: -1});
+
+        const maxOrder = lastOrder ? lastOrder.order : 0;
+
+        const order = await new Order({
+            list: req.body.list,
+            user: req.user.id,
+            order: maxOrder + 1
+        }).save();
+
+        res.status(201).json(order);
+    } catch (err) {
+        errorHandler(res, err);
+    }
+};
 
