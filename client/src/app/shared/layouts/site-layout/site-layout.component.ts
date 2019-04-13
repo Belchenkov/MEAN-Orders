637603b6cdd91cf410f1e6661c743fbc945400e9
Aleksey Belchenkov <u608110@gmail.com>
2019-04-13 17:21:01 +0300

    Add SideBar

--- client/src/app/shared/layouts/site-layout/site-layout.component.ts
+++ client/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-site-layout',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SiteLayoutComponent implements OnInit {
 
-  constructor() { }
+  navLinks = [
+    { url: '/overview', name: 'Обзор', icon: 'remove_red_eye' },
+    { url: '/analytics', name: 'Аналитика', icon: 'trending_up' },
+    { url: '/history', name: 'История', icon: 'today' },
+    { url: '/order', name: 'Добавить заказ', icon: 'add_shopping_cart' },
+    { url: '/categories', name: 'Ассортимент', icon: 'restaurant_menu' }
+  ];
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  logout(event: Event) {
+    event.preventDefault();
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
 }
