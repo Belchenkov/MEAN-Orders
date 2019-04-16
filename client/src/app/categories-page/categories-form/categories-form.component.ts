14b6699d527af80393c281cf4d546bedd95fd4d9
Aleksey Belchenkov <u608110@gmail.com>
2019-04-16 05:52:23 +0300

    Output Data for Edit Category

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,23 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {switchMap} from "rxjs/operators";
 
-@Component({
-  selector: 'app-categories-form',
-  templateUrl: './categories-form.component.html',
-  styleUrls: ['./categories-form.component.scss']
-})
-export class CategoriesFormComponent implements OnInit {
-  isNew: boolean = true;
+import {CategoriesService} from "../../shared/services/categories.service";
+import {of} from "rxjs";
+import {MaterialService} from "../../shared/classes/material.service";
 
-  constructor(private route: ActivatedRoute) { }
+  @Component({
+    selector: 'app-categories-form',
+    templateUrl: './categories-form.component.html',
+    styleUrls: ['./categories-form.component.scss']
+  })
+  export class CategoriesFormComponent implements OnInit {
+    isNew: boolean = true;
+    form: FormGroup;
 
-  ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
-      if (params['id']) {
-        // Edit Category
-        this.isNew = false;
-      }
-    })
+    constructor(
+      private route: ActivatedRoute,
+      private categoryService: CategoriesService
+      ) { }
+
+    ngOnInit() {
+      this.form = new FormGroup({
+        name: new FormControl(null, Validators.required)
+      });
+
+      // Old pattern
+      /*this.route.params.subscribe((params: Params) => {
+        if (params['id']) {
+          // Edit Category
+          this.isNew = false;
+        }
+      });*/
+
+      this.form.disable();
+
+      this.route.params
+        .pipe(
+          switchMap(
+            (params: Params) => {
+                      if (params['id']) {
+                        this.isNew = false;
+                        return this.categoryService.getById(params['id']);
+                      }
+                      return of(null);
+            }
+          )
+        )
+        .subscribe(
+          category => {
+            if (category) {
+              this.form.patchValue({
+                name: category.name
+              });
+              MaterialService.updateTextFields();
+            }
+            this.form.enable();
+          },
+          error => MaterialService.toast(error.error.message)
+        )
+    }
+
+    onSubmit() {
+
+    }
+
   }
-
-}
--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -10,6 +10,10 @@ export class MaterialService {
   static initializeFloatingButton(ref: ElementRef) {
     M.FloatingActionButton.init(ref.nativeElement);
   }
+
+  static updateTextFields() {
+    M.updateTextFields();
+  }
 }
 
 
