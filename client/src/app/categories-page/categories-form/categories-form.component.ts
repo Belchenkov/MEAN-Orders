dca9cce889606a239201baf94cd86744d7de6b90
Aleksey Belchenkov <u608110@gmail.com>
2019-04-17 07:21:14 +0300

    Uploads Image && Create/Updare Category

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {switchMap} from "rxjs/operators";
+import {of} from "rxjs";
 
 import {CategoriesService} from "../../shared/services/categories.service";
-import {of} from "rxjs";
 import {MaterialService} from "../../shared/classes/material.service";
+import {Category} from "../../shared/interfaces";
 
   @Component({
     selector: 'app-categories-form',
@@ -15,6 +16,10 @@ import {MaterialService} from "../../shared/classes/material.service";
   export class CategoriesFormComponent implements OnInit {
     isNew: boolean = true;
     form: FormGroup;
+    @ViewChild('inputFile') inputRef: ElementRef;
+    image: File;
+    imagePreview;
+    category: Category;
 
     constructor(
       private route: ActivatedRoute,
@@ -49,11 +54,13 @@ import {MaterialService} from "../../shared/classes/material.service";
           )
         )
         .subscribe(
-          category => {
+          (category: Category) => {
             if (category) {
+              this.category = category;
               this.form.patchValue({
                 name: category.name
               });
+              this.imagePreview = category.imageSrc;
               MaterialService.updateTextFields();
             }
             this.form.enable();
@@ -62,8 +69,47 @@ import {MaterialService} from "../../shared/classes/material.service";
         )
     }
 
-    onSubmit() {
+    triggerClick() {
+      this.inputRef.nativeElement.click();
+    }
 
+    onFileUpload(event: any) {
+      const file = event.target.files[0];
+      this.image = file;
+
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+
+    onSubmit() {
+      let obs$;
+
+      this.form.disable();
+
+      if (this.isNew) {
+        // create
+        obs$ = this.categoryService.create(this.form.value.name, this.image);
+      } else {
+        // update
+        obs$ = this.categoryService.update(this.category._id, this.form.value.name, this.image);
+      }
+
+      obs$.subscribe(
+        category => {
+          this.category = category;
+          MaterialService.toast('Изменения сохранены!');
+          this.form.enable();
+        },
+        error => {
+          MaterialService.toast(error.error.message);
+          this.form.enable();
+        }
+      )
     }
 
   }
