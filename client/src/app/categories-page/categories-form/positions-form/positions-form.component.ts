ea2dd823b34f989d908c961d1fd2f786e31c30a4
Aleksey Belchenkov <u608110@gmail.com>
2019-04-18 07:09:58 +0300

    Fetch All Position of Category

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+
+import {PositionsService} from "../../../shared/services/positions.service";
+import {Position} from "../../../shared/interfaces";
 
 @Component({
   selector: 'app-positions-form',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./positions-form.component.scss']
 })
 export class PositionsFormComponent implements OnInit {
+  @Input('categoryId') categoryId: string;
+  positions: Position[] = [];
+  loading: boolean = false;
 
-  constructor() { }
+  constructor(private positionService: PositionsService) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.positionService.fetch(this.categoryId)
+      .subscribe(positions => {
+        this.positions = positions;
+        this.loading = false;
+      });
   }
-
 }
