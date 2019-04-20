e369f6a12d1b7e71f2706c5d43a2e4c3bed9f302
Aleksey Belchenkov <u608110@gmail.com>
2019-04-20 21:17:59 +0300

    Output List of Products

--- client/src/app/order-page/order-positions/order-positions.component.ts
+++ client/src/app/order-page/order-positions/order-positions.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {Observable} from "rxjs";
+import {map, switchMap} from "rxjs/operators";
+
+import {PositionsService} from "../../shared/services/positions.service";
+import {Position} from "../../shared/interfaces";
+import {OrderService} from "../order.service";
 
 @Component({
   selector: 'app-order-positions',
@@ -6,10 +13,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-positions.component.scss']
 })
 export class OrderPositionsComponent implements OnInit {
+  positions$: Observable<Position[]>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private positionsService: PositionsService,
+              private order: OrderService
+              ) { }
 
   ngOnInit() {
+    this.positions$ = this.route.params
+      .pipe(
+        switchMap(
+          (params: Params) => {
+            return this.positionsService.fetch(params['id'])
+          }
+        ),
+        map(
+          (positions: Position[]) => {
+            return positions.map(position => {
+              position.quantity = 1;
+              return position;
+            })
+          }
+        )
+      )
+  }
+
+  addToOrder(position: Position) {
+    this.order.add(position);
   }
 
 }
