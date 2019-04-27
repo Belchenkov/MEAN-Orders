f8627b98f434c9f781993be003f66cb981d0638f
Aleksey Belchenkov <u608110@gmail.com>
2019-04-27 12:03:47 +0300

    Paint Charts

--- client/src/app/analytics-page/analytics-page.component.ts
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import {Subscription} from "rxjs";
+import { Chart } from 'chart.js';
 
 import {AnalyticsService} from "../shared/services/analytics.service";
 import {AnalyticsPage} from "../shared/interfaces";
@@ -20,8 +21,34 @@ export class AnalyticsPageComponent implements AfterViewInit, OnDestroy {
   constructor(private service: AnalyticsService) { }
 
   ngAfterViewInit() {
+    const gainConfig: any = {
+      label: 'Выручка',
+      color: 'rgb(255, 99, 132)'
+    };
+
+    const orderConfig: any = {
+      label: 'Заказы',
+      color: 'rgb(54, 162, 235)'
+    };
+
     this.aSub = this.service.getAnalytics().subscribe((data: AnalyticsPage) => {
       this.average = data.average;
+
+      gainConfig.labels = data.chart.map(item => item.label);
+      gainConfig.data = data.chart.map(item => item.gain);
+
+      orderConfig.labels = data.chart.map(item => item.label);
+      orderConfig.data = data.chart.map(item => item.order);
+
+      const gainCtx = this.gainRef.nativeElement.getContext('2d');
+      gainCtx.canvas.height = '300px';
+
+      const orderCtx = this.orderRef.nativeElement.getContext('2d');
+      orderCtx.canvas.height = '300px';
+
+      new Chart(gainCtx, createChartConfig(gainConfig));
+      new Chart(orderCtx, createChartConfig(orderConfig));
+
       this.pending = false;
     })
   }
@@ -31,5 +58,25 @@ export class AnalyticsPageComponent implements AfterViewInit, OnDestroy {
       this.aSub.unsubscribe();
     }
   }
+}
 
+function createChartConfig({labels, data, label, color}) {
+  return {
+    type: 'line',
+    options: {
+      responsive: true
+    },
+    data: {
+      labels,
+      datasets: [
+        {
+          label,
+          data,
+          borderColor: color,
+          steppedLine: false,
+          fill: false
+        }
+      ]
+    }
+  };
 }
